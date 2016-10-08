7db5443a18e155577dc9a750932439cd850cb0f1
grreuze <grreuze@gmail.com>
2016-10-08 21:12:47 +0200

    Add files via upload

---
+++ Scripts/BlinkingText.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour {
+
+    [SerializeField]
+    float blinkTime = 1, waitTime = 0.5f;
+
+	void Start() {
+        StartCoroutine(Idle());
+	}
+	
+    IEnumerator Idle() {
+        Text text = GetComponent<Text>();
+        bool isOn = false;
+        float elapsed = 0;
+        float alpha;
+        while(!Input.GetKeyUp(KeyCode.Space)){
+            elapsed += Time.deltaTime;
+            if (elapsed >= blinkTime) {
+                yield return new WaitForSeconds(waitTime);
+                elapsed = 0;
+                isOn ^= true;
+            }
+            
+            if (isOn) 
+                alpha = Mathf.Lerp(1, 0, elapsed / blinkTime);
+            else 
+                alpha = Mathf.Lerp(0, 1, elapsed / blinkTime);
+
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            yield return null;
+        }
+        DestroyImmediate(gameObject);
+    }
+
+}
