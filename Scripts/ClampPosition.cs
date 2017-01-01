cb0d5c7a0d305152e316c1fc857fed327b0d38b2
grreuze <grreuze@gmail.com>
2017-01-01 22:57:46 +0100

    Add files via upload

---
+++ Scripts/ClampPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ClampPosition : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 min = Vector3.zero, max = Vector3.zero;
+    [SerializeField]
+    bool useLocalPosition = false, drawGizmo = true;
+    
+    Transform _transform;
+
+    void Awake() {
+        _transform = transform;
+    }
+
+    Vector3 position {
+        set {
+            if (useLocalPosition) _transform.localPosition = value;
+            else _transform.position = value;
+        }
+    }
+
+    void LateUpdate() {
+        float x = Mathf.Clamp(_transform.position.x, min.x, max.x);
+        float y = Mathf.Clamp(_transform.position.y, min.y, max.y);
+        float z = Mathf.Clamp(_transform.position.z, min.z, max.z);
+
+        position = new Vector3(x, y, z);
+    }
+
+    void OnDrawGizmos() {
+        if (drawGizmo) {
+            Gizmos.color = Color.cyan;
+
+            Vector3 wireCubePosition = (max + min) / 2;
+            Vector3 wireCubeSize = max - min;
+
+            Gizmos.DrawWireCube(wireCubePosition, wireCubeSize);
+        }
+    }
+}
---
+++ Scripts/FollowTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    /// <summary>
+    /// The transform the FollowTarget object is following.
+    /// </summary>
+    public Transform target;
+    /// <summary>
+    /// The distance between the FollowTarget object and its target.
+    /// </summary>
+    [HideInInspector]
+    public Vector3 distance;
+    Transform _transform;
+
+	void Start () {
+        _transform = transform;
+        distance = target.position + _transform.position;
+	}
+	
+	void Update () {
+        _transform.position = target.position + distance;
+	}
+}
