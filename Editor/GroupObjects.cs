0c4a76f3fea1b8bf7e431bad8463dc473332ac0b
grreuze <grreuze@gmail.com>
2017-04-19 22:57:21 +0200

    Better Local Space Support
    Groups can be made within other groups,
    Groups can be made with objects from various different groups without any position mistakes,
    User can now select whether they want the group in the center of its children or in a custom position.

--- Editor/GroupObjects.cs
+++ Editor/GroupObjects.cs
@@ -3,7 +3,9 @@ using UnityEditor;
 
 public class GroupObjects : EditorWindow {
 
-    string GroupName = "Group";
+    string groupName = "Group";
+    bool centerGroup = true, starting = true;
+    Vector3 groupPosition = Vector3.zero;
 
     [MenuItem("GameObject/Group Objects %G", false, -10)]
     public static void ShowWindow() {
@@ -15,8 +17,13 @@ public class GroupObjects : EditorWindow {
         Event e = Event.current;
         EditorGUILayout.LabelField("Chose Parent name:", EditorStyles.boldLabel);
         GUI.SetNextControlName("TextField");
-        GroupName = GUILayout.TextField(GroupName, GUILayout.Width(200));
-        EditorGUI.FocusTextInControl("TextField");
+        groupName = GUILayout.TextField(groupName, GUILayout.Width(200));
+        if (starting) {
+            EditorGUI.FocusTextInControl("TextField");
+            starting = false;
+        }
+        centerGroup = GUILayout.Toggle(centerGroup, "Center Parent Object");
+        groupPosition = centerGroup ? Vector3.zero : EditorGUILayout.Vector3Field("Parent Object Position", groupPosition);
         if (GUILayout.Button("Create Group") || e.isKey && e.keyCode == KeyCode.Return) {
             Group();
             Close();
@@ -25,16 +32,23 @@ public class GroupObjects : EditorWindow {
     }
 
     void Group() {
-        GameObject newGroup = new GameObject(GroupName);
-
+        GameObject newGroup = new GameObject(groupName);
         Undo.RegisterCreatedObjectUndo(newGroup, "New Group: " + newGroup.name);
-        Vector3 pos = Vector3.zero;
-        
-        foreach (GameObject go in Selection.gameObjects)
-            pos += go.transform.localPosition;
 
         if (Selection.gameObjects.Length > 0) {
-            newGroup.transform.position = pos / (Selection.gameObjects.Length);
+            Transform commonParent = Selection.gameObjects[0].transform.parent;
+
+            foreach (GameObject go in Selection.gameObjects) {
+                if (centerGroup) groupPosition += go.transform.position;
+                if (go.transform.parent != commonParent) commonParent = null;
+            }
+            newGroup.transform.parent = commonParent;
+            if (centerGroup) {
+                groupPosition /= (Selection.gameObjects.Length);
+                if (commonParent)
+                    groupPosition = commonParent.InverseTransformPoint(groupPosition);
+            }
+            newGroup.transform.localPosition = groupPosition;
             foreach (GameObject go in Selection.gameObjects)
                 Undo.SetTransformParent(go.transform, newGroup.transform, "GroupObjects");
         }
