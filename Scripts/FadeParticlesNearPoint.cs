2d7eaebd9c35deb1db1fa7f27ff30bfc246f81a4
grreuze <grreuze@gmail.com>
2017-07-21 09:54:51 +0200

    Update FadeParticlesNearPoint.cs

--- Scripts/FadeParticlesNearPoint.cs
+++ Scripts/FadeParticlesNearPoint.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class FadeParticlesNearPoint : MonoBehaviour {
@@ -9,10 +9,15 @@ public class FadeParticlesNearPoint : MonoBehaviour {
 	[SerializeField] float radius;
 	[SerializeField] float startFadeDistance = 3;
 	bool isLocal;
+	bool ignoreX, ignoreY, ignoreZ;
 
 	void Start() {
 		ps = GetComponent<ParticleSystem>();
 		isLocal = ps.main.simulationSpace == ParticleSystemSimulationSpace.Local;
+
+		ignoreX = point.x == Mathf.Infinity;
+		ignoreY = point.y == Mathf.Infinity;
+		ignoreZ = point.z == Mathf.Infinity;
 	}
 
 	void Update() {
@@ -23,15 +28,16 @@ public class FadeParticlesNearPoint : MonoBehaviour {
 		for (int i = 0; i < count; i++) {
 			ParticleSystem.Particle p = particles[i];
 			Vector3 pos = isLocal ? transform.TransformPoint(p.position) : p.position;
-			if (point.x == Mathf.Infinity) point.x = pos.x;
-			if (point.y == Mathf.Infinity) point.y = pos.y;
-			if (point.z == Mathf.Infinity) point.z = pos.z;
+
+			if (ignoreX) point.x = pos.x;
+			if (ignoreY) point.y = pos.y;
+			if (ignoreZ) point.z = pos.z;
 
 			float distance = Vector3.Distance(pos, point);
 
 			if (distance < startFadeDistance + radius) {
 				Color color = p.startColor;
-				color.a = Mathf.Lerp(0, 1, distance / startFadeDistance);
+				color.a = Mathf.Lerp(0, 1, Mathf.Max(0, distance - radius) / (startFadeDistance + radius));
 				p.startColor = color;
 				if (distance < radius)
 					p.remainingLifetime = 0;
