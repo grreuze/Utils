1aaa292b92c76714d47cb34aaa724981f56e241d
grreuze <grreuze@gmail.com>
2018-03-24 13:21:18 +0100

    Add files via upload

---
+++ Editor/SelectChildren.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class SelectChildren : EditorWindow {
+	
+	[MenuItem("GameObject/Select Children %<", false)]
+	public static void SelectTheChildren() {
+		if (Selection.gameObjects.Length > 0) {
+			List<GameObject> children = new List<GameObject>();
+			foreach (GameObject go in Selection.gameObjects) {
+				foreach (Transform child in go.transform)
+					children.Add(child.gameObject);
+			}
+			Selection.objects = children.ToArray();
+		}
+	}
+}
