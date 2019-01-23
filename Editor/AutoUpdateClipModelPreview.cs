0a90402d81a511fd25643b818f406c344c93166b
grreuze <grreuze@gmail.com>
2019-01-23 09:54:26 +0000

    Cleaned up

--- Editor/AutoUpdateClipModelPreview.cs
+++ Editor/AutoUpdateClipModelPreview.cs
@@ -4,91 +4,130 @@ using UnityEditor;
 using System.Reflection;
 using UnityEditor.Animations;
 
-/// <summary>
-/// When selecting an animationClip, automatically updates the model in the preview.
-/// </summary>
-public class AutoUpdateClipModelPreview : Editor {
+namespace jumpship {
+    [InitializeOnLoad]
+    /// <summary>
+    /// When selecting an animationClip, automatically updates the model in the preview.
+    /// </summary>
+    public class AutoUpdateClipModelPreview {
 
-    private void OnEnable() {
-        Selection.selectionChanged -= OnSelect;
-        if (Selection.selectionChanged != null) {
-            foreach (Delegate m in Selection.selectionChanged.GetInvocationList()) {
-                if (m.Method.DeclaringType.FullName == GetType().FullName) {
-                    Selection.selectionChanged -= (Action)m;
+        static AutoUpdateClipModelPreview() {
+            Selection.selectionChanged -= OnSelect;
+            Selection.selectionChanged += OnSelect;
+        }
+
+        static void OnSelect() {
+            EditorApplication.delayCall += UpdateModelInPreviewWindow; // delay the call to wait for the avatarPreview to be loaded
+        }
+
+
+        enum PreviewHolder {
+            Clip, BlendTree, Transition,
+        }
+
+
+        static void UpdateModelInPreviewWindow() {
+            BlendTree blendTree = Selection.activeObject as BlendTree;
+            AnimatorTransitionBase trans = Selection.activeObject as AnimatorTransitionBase;
+            AnimatorState state = Selection.activeObject as AnimatorState;
+
+            ModelImporter importer = state ? GetModelImporterFromMotion(state.motion)
+                                   : trans ? GetModelImporterFromTransition(trans)
+                                   : blendTree ? GetModelImporterFromBlendTreeRecursively(blendTree) 
+                                   : AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(Selection.activeObject)) as ModelImporter;
+
+            if (importer) {
+                GameObject model = (GameObject)AssetDatabase.LoadAssetAtPath(AssetDatabase.GetAssetPath(importer.sourceAvatar), typeof(GameObject));
+                if (model) {
+                    object _preview = GetAvatarPreview(blendTree ? PreviewHolder.BlendTree : trans ? PreviewHolder.Transition : PreviewHolder.Clip);
+                    if (_preview != null) {
+                        Type avatarPreview = Type.GetType("UnityEditor.AvatarPreview,UnityEditor");
+                        avatarPreview.GetMethod("SetPreview", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_preview, new object[] { model });
+
+                        EditorWindow[] allWindows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+                        foreach (EditorWindow window in allWindows) {
+                            if (window.GetType().Name.Contains("Preview")) {
+                                window.Repaint();
+                                return;
+                            } else if (window.GetType().Name.Contains("Inspector"))
+                                window.Repaint();
+                        }
+
+                    }
                 }
             }
         }
-        Selection.selectionChanged += OnSelect;
-    }
+        
+        static object GetAvatarPreview(PreviewHolder holder) {
+            
+            switch (holder) {
+                case PreviewHolder.Clip:
+                    Type clipEditor = GetEditorType("AnimationClipEditor");
+                    UnityEngine.Object[] openEditors = Resources.FindObjectsOfTypeAll(clipEditor);
 
-    private void OnDestroy() {
-        Selection.selectionChanged -= OnSelect;
-    }
+                    if (openEditors.Length > 0)
+                        return GetPrivateVariable("m_AvatarPreview", openEditors[0], clipEditor);
+                    break;
 
-    void OnSelect() {
-        EditorApplication.delayCall += UpdateModelInPreviewWindow; // delay the call to wait for the avatarPreview to be loaded
-    }
+                case PreviewHolder.BlendTree:
+                    Type blendTreeInspector = GetEditorType("BlendTreeInspector");
+                    UnityEngine.Object[] openInspectors = Resources.FindObjectsOfTypeAll(blendTreeInspector);
 
+                    if (openInspectors.Length > 0) {
+                        Type previewBlendTree = GetEditorType("PreviewBlendTree");
+                        var _previewHold = GetPrivateVariable("m_PreviewBlendTree", openInspectors[0], blendTreeInspector);
 
-    void UpdateModelInPreviewWindow() {
-        BlendTree blendTree = Selection.activeObject as BlendTree;
-        ModelImporter importer = blendTree ? GetModelImporterFromBlendTreeRecursively(blendTree) 
-                               : AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(Selection.activeObject)) as ModelImporter;
-        if (importer) {
-            GameObject model = (GameObject)AssetDatabase.LoadAssetAtPath(AssetDatabase.GetAssetPath(importer.sourceAvatar), typeof(GameObject));
-            if (model) {
-                object _preview = GetAvatarPreview(blendTree);
-                if (_preview != null) {
-                    Type avatarPreview = Type.GetType("UnityEditor.AvatarPreview,UnityEditor");
-                    avatarPreview.GetMethod("SetPreview", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_preview, new object[] { model });
-                }
+                        return GetPrivateVariable("m_AvatarPreview", _previewHold, previewBlendTree);
+                    }
+                    break;
+
+                case PreviewHolder.Transition:
+
+                    // TransitionPreview has no connection to anything and doesn't inherit from Object...
+                    return null;
+                    Type transPreview = GetEditorType("TransitionPreview");
+
+                    UnityEngine.Object[] openPreviews = Resources.FindObjectsOfTypeAll(transPreview);
+
+                    if (openPreviews.Length > 0) {
+                        Debug.Log("first try");
+                        return GetPrivateVariable("m_AvatarPreview", openPreviews[0], transPreview);
+                    }
+                    break;
             }
+            return null;
         }
-    }
 
-    object GetAvatarPreview(bool isBlendTree) {
+        static ModelImporter GetModelImporterFromTransition(AnimatorTransitionBase transition) {
+            if (!transition || !transition.destinationState) return null;
 
-        if (isBlendTree) {
-            Type blendTreeInspector = GetEditorType("BlendTreeInspector");
-            UnityEngine.Object[] openInspectors = Resources.FindObjectsOfTypeAll(blendTreeInspector);
+            Motion motion = transition.destinationState.motion;
 
-            if (openInspectors.Length > 0) {
-                Type previewBlendTree = GetEditorType("PreviewBlendTree");
-                var _previewHold = GetPrivateVariable("m_PreviewBlendTree", openInspectors[0], blendTreeInspector);
-
-                return GetPrivateVariable("m_AvatarPreview", _previewHold, previewBlendTree);
-            }
+            return GetModelImporterFromMotion(transition.destinationState.motion);
         }
-        else {
-            Type clipEditor = GetEditorType("AnimationClipEditor");
-            UnityEngine.Object[] openEditors = Resources.FindObjectsOfTypeAll(clipEditor);
 
-            if (openEditors.Length > 0)
-                return GetPrivateVariable("m_AvatarPreview", openEditors[0], clipEditor);
+        static ModelImporter GetModelImporterFromBlendTreeRecursively(BlendTree blendTree) {
+            if (!blendTree || blendTree.children == null || blendTree.children.Length == 0) return null;
+
+            return GetModelImporterFromMotion(blendTree.children[0].motion);
         }
-        return null;
+
+        static ModelImporter GetModelImporterFromMotion(Motion motion) {
+            if (motion is BlendTree)
+                return GetModelImporterFromBlendTreeRecursively(motion as BlendTree);
+            if (motion is AnimationClip)
+                return AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(motion)) as ModelImporter;
+            return null;
+        }
+
+
+        static object GetPrivateVariable(string name, object instance, Type instanceType) {
+            return instanceType.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(instance);
+        }
+
+        static Type GetEditorType(string name) {
+            return Type.GetType(string.Format("UnityEditor.{0},UnityEditor", name));
+        }
+
     }
-
-
-    ModelImporter GetModelImporterFromBlendTreeRecursively(BlendTree blendTree) {
-        if (!blendTree || blendTree.children == null || blendTree.children.Length == 0) return null;
-
-        Motion motion = blendTree.children[0].motion; // get the model from the first child
-
-        if (motion is BlendTree)
-            return GetModelImporterFromBlendTreeRecursively(motion as BlendTree);
-        if (motion is AnimationClip)
-            return AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(motion)) as ModelImporter;
-        return null;
-    }
-
-
-    object GetPrivateVariable(string name, object instance, Type instanceType) {
-        return instanceType.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(instance);
-    }
-
-    Type GetEditorType(string name) {
-        return Type.GetType(string.Format("UnityEditor.{0},UnityEditor", name));
-    }
-
 }
